13f6fb5b5d7ad8bc88e3d1b2ebc3143dbbc52999
Yuning Zhang <sc212yz@leeds.ac.uk>
2023-04-16 08:58:55 +0800

    install and try SDL

---
+++ initSDL.c
@@ -0,0 +1,19 @@
+// Headers
+#include <SDL2/SDL.h>
+#include <stdio.h>
+// Main function
+int main(int argc, char* argv[])
+{
+    // Initialize SDL
+    if(SDL_Init(SDL_INIT_EVERYTHING) == -1)
+        return(1);
+
+    // Delay 2 seconds
+    SDL_Delay(2000);
+    printf("SDL has installed successfully\n");
+    // Quit SDL
+    SDL_Quit();
+
+    // Return
+    return 0;
+}
